0b36dff713a8f43fe1c8019a6fdb71792b46804e
Brendan <bdrusda@gmail.com>
2023-03-31 00:16:42 -0400

    Added dropdowns for exercise types, default states, worked out how to make add query actually work

--- client/src/components/exercise/ExerciseInput.tsx
+++ client/src/components/exercise/ExerciseInput.tsx
@@ -6,31 +6,32 @@ import Label from '../generics/Label';
 const ExerciseInput = () => {
 	const [name, setName] = useState('');
 	const [description, setDescription] = useState('');
-	const [pushPull, setPushPull] = useState('');
-	const [upperLower, setUpperLower] = useState('');
-	const [bodyPart, setBodyPart] = useState('');
+	const [pushPull, setPushPull] = useState('PUSH');
+	const [upperLower, setUpperLower] = useState('UPPER');
+	const [bodyPart, setBodyPart] = useState('ABS');
 
 	const onSubmitForm = async (e: FormEvent) => {
 		e.preventDefault();
 		try {
+			const exerciseInput = {
+				name: name,
+				description: description,
+				pushPull: pushPull,
+				upperLower: upperLower,
+				bodyPart: bodyPart,
+			};
 			const promise = await fetch(Constants.baseUrl, {
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify({
 					query: `
 						mutation addExercise($input: ExerciseInput){
-							add(input: {input: $input}) {
+							add(input: $input) {
 								id
 							}
 						}
 					`,
-					variables: {
-						name: name,
-						description: description,
-						pushPull: pushPull,
-						upperLower: upperLower,
-						bodyPart: bodyPart,
-					},
+					variables: { input: exerciseInput },
 				}),
 			});
 			const response = await promise.json();
@@ -48,24 +49,56 @@ const ExerciseInput = () => {
 			<div className='exerciseAdd softEdges'>
 				<div className='heading'>Add an exercise</div>
 				<form className='exerciseForm' onSubmit={onSubmitForm}>
-					<Label {...{ name: 'Name', id: 'exerciseAddNameLabel' }} />
-					<input
-						type='text'
-						className='form-control'
-						id='exerciseAddName'
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-					/>
-					<Label
-						{...{ name: 'Description', id: 'exerciseAddDescriptionLabel' }}
-					/>
-					<input
-						type='text'
-						className='form-control'
-						value={description}
-						onChange={(e) => setDescription(e.target.value)}
-					/>
-					<button className='button addButton'>Add</button>
+					<div className='coreInfo'>
+						<Label {...{ name: 'Name', id: 'exerciseAddNameLabel' }} />
+						<input
+							type='text'
+							className='form-control'
+							id='exerciseAddName'
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+						<Label
+							{...{ name: 'Description', id: 'exerciseAddDescriptionLabel' }}
+						/>
+						<input
+							type='text'
+							className='form-control'
+							value={description}
+							onChange={(e) => setDescription(e.target.value)}
+						/>
+					</div>
+					<div className='auxiliaryInfo'>
+						<select
+							className='pushPull dropdown'
+							onChange={(e) => setPushPull(e.target.value)}>
+							<option value='PUSH'>Push</option>
+							<option value='PULL'>Pull</option>
+						</select>
+						<select
+							className='upperLower dropdown'
+							onChange={(e) => setUpperLower(e.target.value)}>
+							<option value='UPPER'>Upper</option>
+							<option value='LOWER'>Lower</option>
+							<option value='CORE'>Core</option>
+						</select>
+						<select
+							className='bodyPart dropdown'
+							onChange={(e) => setBodyPart(e.target.value)}>
+							<option value='ABS'>Abs</option>
+							<option value='BACK'>Back</option>
+							<option value='BIS'>Biceps</option>
+							<option value='CALVES'>Calves</option>
+							<option value='CHEST'>Chest</option>
+							<option value='HAMS'>Hamstrings</option>
+							<option value='QUADS'>Quads</option>
+							<option value='SHOULDERS'>Shoulders</option>
+							<option value='TRIS'>Triceps</option>
+						</select>
+					</div>
+					<div className='addButtonDiv'>
+						<button className='button addButton'>Add</button>
+					</div>
 				</form>
 			</div>
 		</Fragment>
